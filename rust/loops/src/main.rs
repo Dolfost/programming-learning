b4f80a309258d43580b4b7f352cb8141295e0008
Vladyslav <rehanvladyslav@gmail.com>
2025-12-08 18:10:34 +0200

    Add more rust things

---
+++ rust/loops/src/main.rs
@@ -0,0 +1,37 @@
+fn main() {
+    loop {
+        println!("again!");
+        break;
+    }
+
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {result}");
+
+    let mut count = 0;
+    'couting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'couting_up;
+            }
+            remaining -= 1;
+        }
+        count += 1;
+    }
+    println!("End count = {count}");
+}
